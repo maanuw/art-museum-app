f794ab056216ec57d662968cdd4d30b362ebff09
maanuw <59722064+maanuw@users.noreply.github.com>
2023-06-30 14:25:57 -0400

    Updating home page to include navbar buttons

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,8 +6,8 @@
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
-import DefaultButton from '../components/DefaultButton';
-import NavigationBar from '../components/NavigationBar';
+import Button from '../components/Button';
+import Navbar from '../components/Navbar';
 
 import '../styles/pages/Home.css';
 
@@ -15,21 +15,23 @@ import '../styles/pages/Home.css';
 function Home() {
     return (
         <Container fluid>
-            <Row>
+            <Row className='inner-container'>
                 <Col className="intro-txt">
+                    <Navbar/>
+                    {/*}
                     <span className='learn-more'>
-                        <DefaultButton text={"Gallery"}/>
+                        <Button text={"Gallery"} className={'nav-btn'}/>
                     </span>
                     <span className='learn-more-2'>
-                        <DefaultButton text={"About"}/>
+                        <Button text={"About"} className={'nav-btn'}/>
                     </span>
                     <span className='learn-more-2'>
-                        <DefaultButton text={"Donate"}/>
-                    </span>
+                        <Button text={"Donate"} className={'nav-btn'}/>
+    </span>*/}
                     <h1 className='title'>Art With You</h1>
                     <p>Welcome to our Non-profit Virtual Museum, a digital haven for art enthusiasts. Immerse yourself in the captivating world of art, where history, creativity, and technical mastery intertwine. Explore a curated collection of artworks, from classical masterpieces to contemporary expressions. Gain insights into their historical significance, cultural context, and technical intricacies. Engage in conversations and share perspectives in our vibrant comment section, fostering a global community of art appreciation. Your contributions support our mission to preserve and showcase these extraordinary works, making art accessible to all. Join us today and let the power of art inspire your journey of exploration and self-discovery.</p>
                     <span className='learn-more'>
-                        <DefaultButton text={"Learn More"}/>
+                        <Button text={"Learn More"} className={"default-btn"}/>
                     </span>
                 </Col>
                 <Col xs={8} className="Hero"></Col>
